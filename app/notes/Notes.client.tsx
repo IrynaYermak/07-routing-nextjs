fc6bdc6d846ade92bb86fa192c03956fe789102c
IRYNA YERMAK <1991irenka@gmail.com>
2025-12-04 09:06:11 +0000

    Initial commit

---
+++ app/notes/Notes.client.tsx
@@ -0,0 +1,77 @@
+'use client';
+import NoteList from '@/components/NoteList/NoteList';
+import Pagination from '@/components/Pagination/Pagination';
+import SearchBox from '@/components/SearchBox/SearchBox';
+import Modal from '@/components/Modal/Modal';
+import NoteForm from '@/components/NoteForm/NoteForm';
+import Error from '@/components/Error/Error';
+import useModalControl from '@/hooks/useModalControl';
+import { fetchNotes } from '@/lib/api';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import { useState, useEffect } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
+import { toast, Toaster } from 'react-hot-toast';
+import css from './NotesPage.module.css';
+
+export default function NotesClient() {
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('');
+  const { isModalOpen, openModal, closeModal } = useModalControl();
+  const {
+    data: response,
+    isSuccess,
+    isError,
+    isLoading,
+    isFetching,
+  } = useQuery({
+    queryKey: ['notes', search, page],
+    queryFn: () => fetchNotes({ search, page }),
+    enabled: page !== 0,
+    placeholderData: keepPreviousData,
+    refetchOnMount: false,
+  });
+  const totalPages = response?.totalPages ?? 0;
+
+  useEffect(() => {
+    if (response?.notes.length === 0) {
+      toast.error('No notes found for your request.');
+    }
+  }, [response?.notes.length]);
+
+  const hendleSearch = useDebouncedCallback((value: string) => {
+    setSearch(value);
+    setPage(1);
+  }, 300);
+
+  return (
+    <>
+      <section className={css.app}>
+        <Toaster />
+        <div className={css.toolbar}>
+          <SearchBox
+            search={search}
+            onChange={e => hendleSearch(e.target.value)}
+          />
+          {totalPages > 0 && (
+            <Pagination
+              totalPages={totalPages}
+              page={page}
+              onPageChange={setPage}
+            />
+          )}
+          <button className={css.button} onClick={openModal}>
+            Create note +
+          </button>
+        </div>
+        {/* {isLoading || (isFetching && <Loader />)} */}
+        {isError && <Error />}
+        {isSuccess && <NoteList notes={response.notes} />}
+        {isModalOpen && (
+          <Modal onClose={closeModal}>
+            <NoteForm onSuccessClose={closeModal} />
+          </Modal>
+        )}
+      </section>
+    </>
+  );
+}
